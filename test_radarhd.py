09daea1bc287b66f5ad1500730e5e1a60b05cdbe
Andreas Zeck <mail@azeck.info>
2026-02-25 16:57:32 -0600

    feat(01-04): support epoch_num=-1 to load best.pt_gen in test_radarhd.py
    Adds best-checkpoint inference support: when epoch_num is set to -1,
    test_radarhd.py loads best.pt_gen (best training loss checkpoint) instead
    of a specific epoch checkpoint. Output filenames use 'best_' prefix.

--- test_radarhd.py
+++ test_radarhd.py
@@ -9,19 +9,21 @@ import torch
 
 import numpy as np
 from torchinfo import summary
+from PIL import Image
 
 from train_test_utils.dataloader import *
 from train_test_utils.model import *
 
 """
 ## Constants. Edit this to change the model to test on.
+Set epoch_num to -1 to load best.pt_gen (best checkpoint by training loss).
 """
 
 params = {
     'model_name': '13',
     'expt': 1,
     'dt': '20220320-034822',
-    'epoch_num': 120,
+    'epoch_num': 120,   # Set to -1 to use best.pt_gen
     'data': 5,
     'gpu': 1,
 }
@@ -35,7 +37,7 @@ def dataloader(train_params):
 
     test_set = Dataset(basepath, 'test',
                         RBINS=reqd_size[0], ABINS_RADAR=reqd_size[1], ABINS_LIDAR=reqd_size[2],
-                        RBINS_ORIG=orig_size[0], ABINS_RADAR_ORIG=orig_size[1], ABINS_LIDAR_ORIG=orig_size[2], 
+                        RBINS_ORIG=orig_size[0], ABINS_RADAR_ORIG=orig_size[1], ABINS_LIDAR_ORIG=orig_size[2],
                         M=train_params['history'])
     test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=False)
 
@@ -45,7 +47,7 @@ def dataloader(train_params):
 
 def main():
     print(torch.__version__)
-    torch.manual_seed(0)  
+    torch.manual_seed(0)
 
     # Can be set to cuda/cpu. Make sure model and data are moved to cuda if cuda is used
     if params['gpu'] == 1:
@@ -65,8 +67,16 @@ def main():
     gen = UNet1(train_params['history']+1, 1).to(device)
     summary(gen, input_size=(1, train_params['history']+1, 256, 64))
 
-    epoch_num = '%03d' % params['epoch_num']
-    model_file = LOG_DIR + epoch_num + '.pt_gen'
+    # Load checkpoint: epoch_num=-1 loads best.pt_gen
+    if params['epoch_num'] == -1:
+        model_file = os.path.join(LOG_DIR, 'best.pt_gen')
+        epoch_str = 'best'
+        print(f'Loading best checkpoint: {model_file}')
+    else:
+        epoch_str = '%03d' % params['epoch_num']
+        model_file = LOG_DIR + epoch_str + '.pt_gen'
+        print(f'Loading epoch checkpoint: {model_file}')
+
     checkpoint = torch.load(model_file, map_location=device)
     gen.load_state_dict(checkpoint['state_dict'])
 
@@ -83,20 +93,20 @@ def main():
         test_data, test_label = test_data.to(device), test_label.to(device)
         with torch.no_grad():
             pred = gen(test_data)
-            
+
             pred = np.squeeze(pred.cpu().numpy())
             pred = (pred*255).astype(np.uint8)
             im1 = Image.fromarray(pred)
 
-            im1_file_name = save_path + epoch_num + '_' + ordered_filename[test_i] + '_pred.png'
+            im1_file_name = save_path + epoch_str + '_' + ordered_filename[test_i] + '_pred.png'
             im1.save(im1_file_name)
-            
+
             label = np.squeeze(test_label.cpu().numpy())
             label = (label*255).astype(np.uint8)
             im1 = Image.fromarray(label)
-            im1_file_name = save_path + epoch_num + '_' + ordered_filename[test_i] + '_label.png'
+            im1_file_name = save_path + epoch_str + '_' + ordered_filename[test_i] + '_label.png'
             im1.save(im1_file_name)
-            
+
             print(ordered_filename[test_i])
 
     t1 = time.time()
